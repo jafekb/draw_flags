0da4247211902539f148d24fae2cab888f2a4bd0
Ben Jafek <bjafek@nuro.ai>
2025-01-03 16:55:32 -0500

    Just remove the old flag_searcher

--- src/utils.py
+++
@@ -1,6 +0,0 @@
-# TODO(bjafek) make a constants.py instead
-ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
-
-def allowed_file(filename):
-    return '.' in filename and \
-            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
