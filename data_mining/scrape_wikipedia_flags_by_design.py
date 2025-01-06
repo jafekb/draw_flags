c8e105610d391a0d64bb7ce9b8c15b9c13d30279
Ben Jafek <bjafek@nuro.ai>
2025-01-05 22:59:26 -0800

    Now works to step through all the gallery boxes and get the flags

--- data_mining/scrape_wikipedia_flags_by_design.py
+++ data_mining/scrape_wikipedia_flags_by_design.py
@@ -9,8 +9,31 @@ import time
 
 from selenium import webdriver
 import chromedriver_binary  # Adds chromedriver binary to path
+from selenium.webdriver.common.by import By
+
 
 driver = webdriver.Chrome()
-driver.get("https://commons.wikimedia.org/wiki/Gallery_of_flags_by_design")
+# TODO(bjafek) this one doesn't load fully unless you scroll down, how?
+# driver.get("https://commons.wikimedia.org/wiki/Gallery_of_flags_by_design")
+driver.get("https://en.wikipedia.org/wiki/List_of_flags_by_design")
 
-time.sleep(5)
+# Find all elements with the given class and style attributes
+gallery_boxes = driver.find_elements(
+    By.XPATH,
+    "//li[@class='gallerybox' and contains(@style, 'width: 185px')]",
+)
+
+# Print the number of elements found
+print(f"Number of gallery boxes found: {len(gallery_boxes)}")
+
+time.sleep(2)
+
+# Iterate through the found elements
+for box in gallery_boxes:
+    # Perform actions on each element, e.g.,
+    print(box.text)  # Print the text content of the element
+    # box.click()  # Click on the element
+    # time.sleep(2)
+
+# Close the browser
+driver.quit()
