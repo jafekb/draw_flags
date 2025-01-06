7b73c4cec0f910d2da79923d1ef121c9479f335e
Ben Jafek <bjafek@nuro.ai>
2025-01-06 12:43:18 -0800

    Better way to scrape from wikipedia using their official API and the wikipedia pip package

---
+++ data_mining/get_info_from_wikipedia_flags.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+"""
+Scrape wikipedia using the `wikipedia` pip package
+"""
+import wikipedia
+
+page = wikipedia.page("List of flags by design")
+
+# TODO(bjafek) do stuff, get images
