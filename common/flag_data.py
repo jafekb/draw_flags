a290a0ce0ddffbc15d466263da58224295cc2a24
Ben Jafek <bjafek@nuro.ai>
2025-01-11 21:32:25 -0800

    Beef up the Flag dataclass a little bit more

--- common/flag_data.py
+++ common/flag_data.py
@@ -2,9 +2,11 @@
 Utils file for the FlagSearcher & backend.
 """
 
+import json
+from pathlib import Path
 from typing import List
 
-from pydantic import BaseModel
+from pydantic import BaseModel, validator
 
 
 class Flag(BaseModel):
@@ -14,11 +16,39 @@ class Flag(BaseModel):
 
     name: str
     wikipedia_page: str
-    wikipedia_link: str
-    wikipedia_image_link: str
+    wikipedia_url: str
+    wikipedia_image_url: str
     local_image_link: str
-    verification_method: str
+    verification_method: str = ""
     score: float = 0.0
+
+    @validator("verification_method")
+    def validate_my_field(cls, v):
+        allowed_values = {"check_options", "commons", "table"}
+        if v not in allowed_values:
+            raise ValueError(f"Invalid 'verification_method'. Allowed values are: {allowed_values}")
+        return v
+
+    def to_json(self, out_dir: Path) -> None:
+        if not out_dir.is_dir():
+            raise ValueError(f"out_dir must be a valid directory! got '{out_dir}'")
+        out_name = out_dir / f"{self.name}.json"
+
+        with out_name.open("w") as f:
+            json.dump(self.dict(), f, indent=1)
+        return
+
+
+def flag_from_json(file_name: Path) -> Flag:
+    if isinstance(file_name, str):
+        print("warning: please use pathlib.Path instead of str :)")
+        file_name = Path(file_name)
+    if not file_name.is_file():
+        raise ValueError(f"File does not exist! {file_name}")
+
+    with file_name.open() as f:
+        data = json.load(f)
+    return Flag(**data)
 
 
 class Flags(BaseModel):
