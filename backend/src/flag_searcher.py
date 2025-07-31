3e1604d7803c4b39054bd985e763ee783fb773f0
Ben Jafek <bjafek@nuro.ai>
2025-07-31 11:23:21 -0700

    replace img with more correct text_query

--- backend/src/flag_searcher.py
+++ backend/src/flag_searcher.py
@@ -25,12 +25,12 @@ class FlagSearcher:
         self._flags = flaglist_from_json(FLAGS_FILE)
         self._encoded_images = np.load(self._flags.embeddings_filename)
 
-    def query(self, img, is_image) -> FlagList:
+    def query(self, text_query, is_image) -> FlagList:
         """
         Run the recognizer, comparing to all the existing stuff.
 
         Arguments:
-            img:
+            text_query:
             is_image (bool): TODO(bjafek) this currently handles both text
                 and image querying.
 
@@ -43,7 +43,7 @@ class FlagSearcher:
             #  gets passed, instead of this junk.
             # fn = "/home/bjafek/personal/draw_flags/examples/" + img.data
             # img = Image.open(fn)
-        new_embedding = self._model.encode([img])
+        new_embedding = self._model.encode([text_query])
         similarity_scores = cosine_similarity(new_embedding, self._encoded_images)
         top_k_indices = similarity_scores.argsort()[0][::-1][: self._top_k]
         sorted_scores = similarity_scores.ravel()[top_k_indices].tolist()
